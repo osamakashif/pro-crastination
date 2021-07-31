fc2ca0fd09a0b122588935a39a955b5f8a073006
uoa-lwu973 <lucywu125@gmail.com>
2021-08-01 10:56:57 +1200

    Icons resize

--- front/src/components/WastingTime.js
+++ front/src/components/WastingTime.js
@@ -10,67 +10,71 @@ function WastingTime (){
                 <title>Analog Clock</title>  
             </header>  
             <body>
-                <div className="Clock"><ana-clock></ana-clock></div>
-                <div className="Clock12">
-                    <a href="https://www.linkedin.com">
-                        <FaLinkedin size='60px'color="B3E5FC"/>
+                <div className="Clock">
+                    <ana-clock></ana-clock>
+                    <div className="Clock12">
+                    <a href="https://www.linkedin.com" target="_blank">
+                        <FaLinkedin size='20%'color="B3E5FC"/>
                     </a>
                 </div>
                 <div className="Clock1">
-                    <a href="https://www.facebook.com">
-                        <FaFacebook size='60px'color="B3E5FC"/>
+                    <a href="https://www.facebook.com" target="_blank">
+                        <FaFacebook size='20%'color="B3E5FC"/>
                     </a>
                 </div>
                 <div className="Clock2">
-                    <a href="https://www.discord.com">
-                        <FaDiscord size='60px'color="B3E5FC"/>
+                    <a href="https://www.discord.com" target="_blank">
+                        <FaDiscord size='30%'color="B3E5FC"/>
                     </a>
                 </div>
                 <div className="Clock3">
-                    <a href="https://www.snapchat.com">
-                        <FaSnapchat size='60px'color="B3E5FC"/>
+                    <a href="https://www.snapchat.com" target="_blank">
+                        <FaSnapchat size='30%'color="B3E5FC"/>
                     </a>
                 </div>
                 <div className="Clock4">
-                    <a href="https://www.pinterest.com">
-                        <FaPinterest size='60px'color="B3E5FC"/>
+                    <a href="https://www.pinterest.com" target="_blank">
+                        <FaPinterest size='30%'color="B3E5FC"/>
                     </a>
                 </div>
                 <div className="Clock5">
-                    <a href="https://www.reddit.com">
-                        <FaReddit size='60px'color="B3E5FC"/>
+                    <a href="https://www.reddit.com" target="_blank">
+                        <FaReddit size='20%'color="B3E5FC"/>
                     </a>
                 </div>
                 <div className="Clock6">
-                    <a href="https://www.spotify.com">
-                        <FaSpotify size='60px'color="B3E5FC"/>
+                    <a href="https://www.spotify.com" target="_blank">
+                        <FaSpotify size='20%'color="B3E5FC"/>
                     </a>
                 </div>
                 <div className="Clock7">
-                    <a href="https://www.youtube.com">
-                        <FaYoutube size='60px'color="B3E5FC"/>
+                    <a href="https://www.youtube.com" target="_blank">
+                        <FaYoutube size='20%'color="B3E5FC"/>
                     </a>
                 </div>
                 <div className="Clock8">
-                    <a href="https://www.twitter.com">
-                        <FaTwitter size='60px'color="B3E5FC"/>
+                    <a href="https://www.twitter.com" target="_blank">
+                        <FaTwitter size='20%'color="B3E5FC"/>
                     </a>
                 </div>
                 <div className="Clock9">
-                    <a href="https://www.instagram.com">
-                        <FaInstagram size='60px' color="B3E5FC"/>
+                    <a href="https://www.instagram.com" target="_blank">
+                        <FaInstagram size='20%' color="B3E5FC"/>
                     </a>
                 </div>
                 <div className="Clock10">
-                    <a href="https://www.whatsapp.com">
-                        <FaWhatsapp size='60px' color="B3E5FC"/>
+                    <a href="https://www.whatsapp.com" target="_blank">
+                        <FaWhatsapp size='20%' color="B3E5FC"/>
                     </a>
                 </div>
                 <div className="Clock11">
-                    <a href="https://www.soundcloud.com">
-                        <FaSoundcloud size='60px'color="B3E5FC"/>
+                    <a href="https://www.soundcloud.com" target="_blank">
+                        <FaSoundcloud size='20%'color="B3E5FC"/>
                     </a>
                 </div>
+                </div>
+                
+                
             </body>
         </div> 
     )
--- front/src/components/WastingTimeMoreEfficiently.js
+++ front/src/components/WastingTimeMoreEfficiently.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './WastingTime.css';
-import {FaWater, FaNewspaper, FaReact, FaWikipediaW, FaUniversity, FaRunning, FaStackOverflow, FaBook, FaGraduationCap, FaPen, FaLanguage, FaDollarSign} from 'react-icons/fa';
+import {FaWater, FaNewspaper, FaReact, FaWikipediaW, FaUniversity, FaRunning, FaGoogleDrive, FaBook, FaGraduationCap, FaPen, FaLanguage, FaDollarSign} from 'react-icons/fa';
 
 function WastingTimeMoreEfficiently (){
     return (
@@ -9,67 +9,71 @@ function WastingTimeMoreEfficiently (){
                 <title>Analog Clock</title>  
             </header>  
             <body>
-                <div className="Clock"><ana-clock></ana-clock></div>
-                <div className="Clock12">
-                    <a href="https://www.wikipedia.com">
-                        <FaWikipediaW size='60px'color="B3E5FC"/>
+                <div className="Clock">
+                    <ana-clock></ana-clock>
+
+                    <div className="Clock12">
+                    <a href="https://en.wikipedia.org/wiki/Special:Random" target="_blank">
+                        <FaWikipediaW size='20%'color="B3E5FC"/>
                     </a>
                 </div>
                 <div className="Clock1">
-                    <a href="https://www.nzherald.co.nz">
-                        <FaNewspaper size='60px'color="B3E5FC"/>
+                    <a href="https://www.nzherald.co.nz" target="_blank">
+                        <FaNewspaper size='20%'color="B3E5FC"/>
                     </a>
                 </div>
                 <div className="Clock2">
-                    <a href="https://www.stackoverflow.com">
-                        <FaStackOverflow size='60px'color="B3E5FC"/>
+                    <a href="https://www.drive.google.com" target="_blank">
+                        <FaGoogleDrive size='30%'color="B3E5FC"/>
                     </a>
                 </div>
                 <div className="Clock3">
-                    <a href="https://reactjs.org/">
-                        <FaReact size='60px'color="B3E5FC"/>
+                    <a href="https://reactjs.org/" target="_blank">
+                        <FaReact size='40%'color="B3E5FC"/>
                     </a>
                 </div>
                 <div className="Clock4">
-                    <a href="https://www.auckland.ac.nz/en.html">
-                        <FaUniversity size='60px'color="B3E5FC"/>
+                    <a href="https://www.auckland.ac.nz/en.html" target="_blank">
+                        <FaUniversity size='30%'color="B3E5FC"/>
                     </a>
                 </div>
                 <div className="Clock5">
-                    <a href="https://www.marketwatch.com/">
-                        <FaDollarSign size='60px'color="B3E5FC"/>
+                    <a href="https://www.marketwatch.com/" target="_blank">
+                        <FaDollarSign size='15%'color="B3E5FC"/>
                     </a>
                 </div>
                 <div className="Clock6">
-                    <a href="https://canvas.auckland.ac.nz/">
-                        <FaPen size='60px'color="B3E5FC"/>
+                    <a href="https://canvas.auckland.ac.nz/" target="_blank">
+                        <FaPen size='20%'color="B3E5FC"/>
                     </a>
                 </div>
                 <div className="Clock7">
-                    <a href="https://www.duolingo.com">
-                        <FaLanguage size='60px'color="B3E5FC"/>
+                    <a href="https://www.duolingo.com" target="_blank">
+                        <FaLanguage size='20%'color="B3E5FC"/>
                     </a>
                 </div>
                 <div className="Clock8">
-                    <a href="https://www.youtube.com/watch?v=ziUCPDv2vBI">
-                        <FaWater size='60px'color="B3E5FC"/>
+                    <a href="https://www.youtube.com/watch?v=ziUCPDv2vBI" target="_blank">
+                        <FaWater size='20%'color="B3E5FC"/>
                     </a>
                 </div>
                 <div className="Clock9">
-                    <a href="https://www.cityfitness.co.nz/memberships">
-                        <FaRunning size='60px' color="B3E5FC"/>
+                    <a href="https://www.cityfitness.co.nz/memberships" target="_blank">
+                        <FaRunning size='20%' color="B3E5FC"/>
                     </a>
                 </div>
                 <div className="Clock10">
-                    <a href="https://scholar.google.com/">
-                        <FaGraduationCap size='60px' color="B3E5FC"/>
+                    <a href="https://scholar.google.com/" target="_blank">
+                        <FaGraduationCap size='20%' color="B3E5FC"/>
                     </a>
                 </div>
                 <div className="Clock11">
-                    <a href="https://www.audible.com.au/">
-                        <FaBook size='60px'color="B3E5FC"/>
+                    <a href="https://www.audible.com.au/" target="_blank">
+                        <FaBook size='15%'color="B3E5FC"/>
                     </a>
                 </div>
+                </div>
+                
             </body>
         </div> 
     )
