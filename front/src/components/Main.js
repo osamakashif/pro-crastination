decc1e7dcba0964b9357a5bf049a7db93d351d34
yuchendong <68779035+uoa-ydon461@users.noreply.github.com>
2021-07-31 15:38:34 +1200

    WIP title screen formatting

--- front/src/components/Main.js
+++ front/src/components/Main.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import logo from './pslogo.png';
+import './Main.css';
 function Main() {
     return (
-    <div className="App">
-        <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>ProcraStation</h1>
-        <p>
-            Your station, where you come to procrastinate. ;)
-        </p>
-        
-        <a
-            className="App-link"
-            href="https://osamakashif.dev"
-            target="_blank"
-            rel="noopener noreferrer"
-        >
-            Osama's website ;)
-        </a>
+    <div className="Main">
+        <header className="Main-header">
+            <img src={logo} className="Main-logo" alt="logo" />
+            <h1 className="Main-title">ProcraStation</h1>
+            <p className="Main-slogan">
+                Your procrastination station. Come now &#60;3 {/* the &#60; is for '<' */}
+            </p>
         </header>
+        <div className="Main-info">
+            <form className="Main-login">
+                <b>Log In</b>
+                <div className="Main-loginName">
+                    <label for="name">Username:</label>
+                    <input type="text" id="name"/>
+                </div>
+                <div className="Main-loginPass">
+                    <label for="pass">Password:</label>
+                    <input type="text"id="pass"/>
+                </div>
+                <button className="Main-button">Button</button>
+            </form>
+            <form className="Main-register">
+                <b>Register</b>
+                <div className="Main-email">
+                    <label for="email">Email:</label>
+                    <input type="text" id="email"/>
+                </div>
+                <div className="Main-registerName">
+                    <label for="name">Username:</label>
+                    <input type="text" id="name"/>
+                </div>
+                <div className="Main-registerPass">
+                    <label for="pass">Password:</label>
+                    <input type="text"id="pass"/>
+                </div>
+                <div className="Main-confirmPass">
+                    <label for="confirmPass">Confirm Password:</label>
+                    <input type="text"id="pass"/>
+                </div>
+                <button className="Main-button">Register</button>
+            </form>
+        </div>
+        <button>Procrastinate</button>
     </div>
     );
 }
