e7ea249321afdc167818be6eedd4f8e8d9df5135
mdar365 <68842181+mdar365@users.noreply.github.com>
2021-07-31 22:59:23 +1200

    login and registration connected to BE

--- back/src/API/connect.js
+++ back/src/API/connect.js
@@ -1,3 +1,4 @@
+const cors = require('cors')
 const express = require('express')
 const MongoClient = require('mongodb').MongoClient;
 const app = express()
@@ -16,6 +17,7 @@ MongoClient.connect('mongodb://localhost:27017/ProcraStation', function (err, da
    }
  });
 
+app.use(cors());
 app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
@@ -25,13 +27,18 @@ app.get('/', (req, res) => {
 //Searches for login in db
 app.post('/login', (req, res) => {
   db.collection('Users').findOne(req.body, function (err, result) {
-    if (err)
-      res.send('Error');
-    else
+    if (err) {
+      res.status(500);
+      res.sendStatus(res.statusCode);
+    } else
       if (result == undefined){
-        res.send("User not found, please sign up")
+        res.status(401)
+        res.sendStatus(res.statusCode)
+        console.log(res.statusCode)
       } else {
-        res.send("login successful")
+        res.status(200)
+        res.sendStatus(res.statusCode)
+        console.log(res.statusCode)
       }
   });
 })
@@ -39,15 +46,20 @@ app.post('/login', (req, res) => {
 //Adds login credentials to db
 app.post('/signup', (req, res) => {
   db.collection('Users').findOne(req.body, function (err, result) {
-    if (err)
-      res.send('Error');
-    else
+    if (err){
+      res.status(500);
+      res.sendStatus(res.statusCode);
+    } else
       if (result == undefined){
         db.collection('Users').insert(req.body, function (err, result) {
-          if (err)
-            res.send('Error');
-          else
-            res.send('Registration Successful');
+          if (err) {
+            res.status(400)
+            res.sendStatus(res.statusCode)
+            console.log(res.statusCode)
+          } else
+            res.status(200)
+            res.sendStatus(res.statusCode)
+            console.log(res.statusCode)
         });
       } else {
         res.send("User already exists")
--- front/src/components/Main.js
+++ front/src/components/Main.js
@@ -2,6 +2,45 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import logo from './logo.svg';
 import './Main.css';
+import axios from 'axios';
+
+//This function is executed when user clicks on login
+async function login(event) {
+    event.preventDefault();
+    try{
+        var url = 'http://localhost:4000/login';
+        const res = await axios.post(url, {username: document.getElementById("loginName").value , password: document.getElementById("loginPass").value});
+        console.log(res.status);
+        window.location = "/Wasting_Choice"
+    } catch (err){
+        alert(err);
+    } finally {
+        document.getElementById("loginName").value = null
+        document.getElementById("loginPass").value = null
+    }
+}
+
+//This function is executed when user clicks on register
+async function register(event) {
+    event.preventDefault();
+    try {
+        var url = 'http://localhost:4000/signup';
+        if(document.getElementById("registerPass").value === document.getElementById("confirmPass").value){
+            const res = await axios.post(url, {username: document.getElementById("registerName").value , password: document.getElementById("registerPass").value});
+            console.log(res.status);
+            alert("Register Successful")
+            window.location = "/Wasting_Choice" 
+        } else {
+            alert("Passwords do not match")
+        }
+    } catch (err){
+        alert(err)
+    } finally {
+        document.getElementById("registerName").value = null
+        document.getElementById("registerPass").value = null
+        document.getElementById("confirmPass").value = null
+    }
+}
 
 function Main() {
     return (
@@ -18,37 +57,34 @@ function Main() {
                 <b>Log In</b>
                 <div className="Main-loginName">
                     <label for="name">Username:&nbsp;&nbsp;</label>
-                    <input type="text" id="name"/>
+                    <input type="text" id="loginName"/>
                 </div>
                 <div className="Main-loginPass">
                     <label for="pass">Password:&nbsp;&nbsp;</label>
-                    <input type="text"id="pass"/>
+                    <input type="text"id="loginPass"/>
                 </div>
-                <button className="Main-loginButton">Log In</button>
+                <button className="Main-loginButton" onClick={login}>Log In</button>
             </form>
             <form className="Main-register">
                 <b>Register</b>
-                <div className="Main-email">
-                    <label for="email">Email:&nbsp;&nbsp;</label>
-                    <input type="text" id="email"/>
-                </div>
                 <div className="Main-registerName">
                     <label for="name">Username:&nbsp;&nbsp;</label>
-                    <input type="text" id="name"/>
+                    <input type="text" id="registerName"/>
                 </div>
                 <div className="Main-registerPass">
                     <label for="pass">Password:&nbsp;&nbsp;</label>
-                    <input type="text"id="pass"/>
+                    <input type="text"id="registerPass"/>
                 </div>
                 <div className="Main-confirmPass">
                     <label for="confirmPass">Confirm Password:&nbsp;&nbsp;</label>
-                    <input type="text"id="pass"/>
+                    <input type="text"id="confirmPass"/>
                 </div>
-                <button className="Main-registerButton">Register</button>
+                <button className="Main-registerButton" onClick={register}>Register</button>
             </form>
         </div>
         <NavLink to="/Wasting_Time"><button className="Main-wasteTime">...or just waste time!</button></NavLink>
     </div>
     );
 }
+
 export default Main;
