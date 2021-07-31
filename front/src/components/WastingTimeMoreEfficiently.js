34c5ce29e9a5fb4452abdddcf17aaaeeef2438a5
yuchendong <68779035+uoa-ydon461@users.noreply.github.com>
2021-08-01 11:59:03 +1200

    WIP clock screen? optional

--- front/src/components/WastingTimeMoreEfficiently.js
+++ front/src/components/WastingTimeMoreEfficiently.js
@@ -7,6 +7,9 @@ function WastingTimeMoreEfficiently (){
         <div className="App">
             <body>
                 <h1>It's Procrastination Time!</h1>
+                <div className="textbox">
+                    Tooltip Box
+                </div>
                 <div className="Clock">
                     <ana-clock></ana-clock>
 
