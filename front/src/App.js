1ba4e62eb9fcadb3db7b045679438d8cafd3b8ec
yuchendong <68779035+uoa-ydon461@users.noreply.github.com>
2021-07-31 14:02:25 +1200

    feat Working Components

--- front/src/App.js
+++ front/src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Main from './components/Main';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>ProcraStation</h1>
-        <p>
-          Your station, where you come to procrastinate. ;)
-        </p>
-        <a
-          className="App-link"
-          href="https://osamakashif.dev"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Osama's website ;)
-        </a>
-      </header>
-    </div>
+    <Main/>
   );
 }
 
---
+++ front/src/components/Main.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+function Main() {
+    return (
+    <div className="App">
+        <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>ProcraStation</h1>
+        <p>
+            Your station, where you come to procrastinate. ;)
+        </p>
+        
+        <a
+            className="App-link"
+            href="https://osamakashif.dev"
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+            Osama's website ;)
+        </a>
+        </header>
+    </div>
+    );
+}
+export default Main;
