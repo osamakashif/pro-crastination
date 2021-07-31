f47cec0d7dce3320f1b8d3372349463289dd4ee6
uoa-lwu973 <lucywu125@gmail.com>
2021-08-01 09:52:13 +1200

    waste time more icons

--- front/src/components/WastingTime.js
+++ front/src/components/WastingTime.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import './WastingTime.css';
-import {IconContext} from "react-icons";
-import {FaTwitter, FaFacebook, FaSnapchat, FaLinkedin, FaReddit, FaInstagram, FaDiscord, FaSoundcloud, FaWhatsapp, FaSpotify, FaYoutube, FaPinterest, FaTumblr} from 'react-icons/fa';
 import { AnaClock, DigiClock } from 'web-clocks';
-import { icons } from 'react-icons/lib';
+import {FaTwitter, FaFacebook, FaSnapchat, FaLinkedin, FaReddit, FaInstagram, FaDiscord, FaSoundcloud, FaWhatsapp, FaSpotify, FaYoutube, FaPinterest} from 'react-icons/fa';
 
 function WastingTime (){
     return (
@@ -12,63 +10,63 @@ function WastingTime (){
                 <title>Analog Clock</title>  
             </header>  
             <body>
-                <div className="clockthing"><ana-clock className="clockthing"></ana-clock></div>
-                <div className="LinkedIn">
+                <div className="Clock"><ana-clock></ana-clock></div>
+                <div className="Clock12">
                     <a href="https://www.linkedin.com">
                         <FaLinkedin size='60px'color="B3E5FC"/>
                     </a>
                 </div>
-                <div className="Facebook">
+                <div className="Clock1">
                     <a href="https://www.facebook.com">
                         <FaFacebook size='60px'color="B3E5FC"/>
                     </a>
                 </div>
-                <div className="Discord">
+                <div className="Clock2">
                     <a href="https://www.discord.com">
                         <FaDiscord size='60px'color="B3E5FC"/>
                     </a>
                 </div>
-                <div className="Snapchat">
+                <div className="Clock3">
                     <a href="https://www.snapchat.com">
                         <FaSnapchat size='60px'color="B3E5FC"/>
                     </a>
                 </div>
-                <div className="Pinterest">
+                <div className="Clock4">
                     <a href="https://www.pinterest.com">
                         <FaPinterest size='60px'color="B3E5FC"/>
                     </a>
                 </div>
-                <div className="Reddit">
+                <div className="Clock5">
                     <a href="https://www.reddit.com">
                         <FaReddit size='60px'color="B3E5FC"/>
                     </a>
                 </div>
-                <div className="Spotify">
+                <div className="Clock6">
                     <a href="https://www.spotify.com">
                         <FaSpotify size='60px'color="B3E5FC"/>
                     </a>
                 </div>
-                <div className="Youtube">
+                <div className="Clock7">
                     <a href="https://www.youtube.com">
                         <FaYoutube size='60px'color="B3E5FC"/>
                     </a>
                 </div>
-                <div className="Tumblr">
-                    <a href="https://www.tumblr.com">
-                        <FaTumblr size='60px'color="B3E5FC"/>
+                <div className="Clock8">
+                    <a href="https://www.twitter.com">
+                        <FaTwitter size='60px'color="B3E5FC"/>
                     </a>
                 </div>
-                <div className="Instagram">
+                <div className="Clock9">
                     <a href="https://www.instagram.com">
                         <FaInstagram size='60px' color="B3E5FC"/>
                     </a>
                 </div>
-                <div className="Whatsapp">
+                <div className="Clock10">
                     <a href="https://www.whatsapp.com">
                         <FaWhatsapp size='60px' color="B3E5FC"/>
                     </a>
                 </div>
-                <div className="Soundcloud">
+                <div className="Clock11">
                     <a href="https://www.soundcloud.com">
                         <FaSoundcloud size='60px'color="B3E5FC"/>
                     </a>
