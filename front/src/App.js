fc2e6fa2a211dc2c96689e16f968e0020a2060b0
osamakashif <osamakashif1@hotmail.com>
2021-07-31 15:17:12 +1200

    feat  - React Router DOM
    Adding React Router DOM

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,19 @@
 
 import './App.css';
 import Main from './components/Main';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <Main/>
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Main />
+        </Route>
+      </ Switch>
+      <Switch>
+      </ Switch>
+    </Router>
   );
 }
 
