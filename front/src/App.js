800f6ed3e19c0c4433885b55912864b0eac786d7
osamakashif <osamakashif1@hotmail.com>
2021-07-31 15:31:49 +1200

    feat - Added new page
    Added WastingChoice page.

--- front/src/App.js
+++ front/src/App.js
@@ -1,17 +1,19 @@
 
 import './App.css';
 import Main from './components/Main';
+import WastingChoice from './components/WastingChoice';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/">
+        <Route exact path="/">
           <Main />
         </Route>
-      </ Switch>
-      <Switch>
+        <Route path="/wasting_choice">
+          <WastingChoice />
+        </Route>
       </ Switch>
     </Router>
   );
---
+++ front/src/components/WastingChoice.js
@@ -0,0 +1,9 @@
+function WastingChoice() {
+    return (
+    <div className="App">
+       <button>Waste Time</button>
+       <button>Waste Time <italic>efficiently</italic></button>
+    </div>
+    );
+}
+export default WastingChoice;
