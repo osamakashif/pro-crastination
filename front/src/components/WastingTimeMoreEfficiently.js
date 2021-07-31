01d94d19eb2afc11413b573e71e191d2b493e061
yuchendong <68779035+uoa-ydon461@users.noreply.github.com>
2021-08-01 11:44:21 +1200

    Fixed clocks

--- front/src/components/WastingTimeMoreEfficiently.js
+++ front/src/components/WastingTimeMoreEfficiently.js
@@ -5,26 +5,24 @@ import {FaWater, FaNewspaper, FaReact, FaWikipediaW, FaUniversity, FaRunning, Fa
 function WastingTimeMoreEfficiently (){
     return (
         <div className="App">
-            <header>  
-                <title>Analog Clock</title>  
-            </header>  
             <body>
+                <h1>It's Procrastination Time!</h1>
                 <div className="Clock">
                     <ana-clock></ana-clock>
 
                     <div className="Clock12">
                     <a href="https://en.wikipedia.org/wiki/Special:Random" target="_blank">
-                        <FaWikipediaW size='20%'color="B3E5FC"/>
+                        <FaWikipediaW size='100%'color="B3E5FC"/>
                     </a>
                 </div>
                 <div className="Clock1">
                     <a href="https://www.nzherald.co.nz" target="_blank">
-                        <FaNewspaper size='20%'color="B3E5FC"/>
+                        <FaNewspaper size='100%'color="B3E5FC"/>
                     </a>
                 </div>
                 <div className="Clock2">
                     <a href="https://www.drive.google.com" target="_blank">
-                        <FaGoogleDrive size='30%'color="B3E5FC"/>
+                        <FaGoogleDrive size='100%'color="B3E5FC"/>
                     </a>
                 </div>
                 <div className="Clock3">
@@ -34,42 +32,42 @@ function WastingTimeMoreEfficiently (){
                 </div>
                 <div className="Clock4">
                     <a href="https://www.auckland.ac.nz/en.html" target="_blank">
-                        <FaUniversity size='30%'color="B3E5FC"/>
+                        <FaUniversity size='100%'color="B3E5FC"/>
                     </a>
                 </div>
                 <div className="Clock5">
                     <a href="https://www.marketwatch.com/" target="_blank">
-                        <FaDollarSign size='15%'color="B3E5FC"/>
+                        <FaDollarSign size='80%'color="B3E5FC"/>
                     </a>
                 </div>
                 <div className="Clock6">
                     <a href="https://canvas.auckland.ac.nz/" target="_blank">
-                        <FaPen size='20%'color="B3E5FC"/>
+                        <FaPen size='100%'color="B3E5FC"/>
                     </a>
                 </div>
                 <div className="Clock7">
                     <a href="https://www.duolingo.com" target="_blank">
-                        <FaLanguage size='20%'color="B3E5FC"/>
+                        <FaLanguage size='100%'color="B3E5FC"/>
                     </a>
                 </div>
                 <div className="Clock8">
                     <a href="https://www.youtube.com/watch?v=ziUCPDv2vBI" target="_blank">
-                        <FaWater size='20%'color="B3E5FC"/>
+                        <FaWater size='100%'color="B3E5FC"/>
                     </a>
                 </div>
                 <div className="Clock9">
-                    <a href="https://www.cityfitness.co.nz/memberships" target="_blank">
-                        <FaRunning size='20%' color="B3E5FC"/>
+                    <a href="https://www.overstellar.se/random-exercise/" target="_blank">
+                        <FaRunning size='100%' color="B3E5FC"/>
                     </a>
                 </div>
                 <div className="Clock10">
                     <a href="https://scholar.google.com/" target="_blank">
-                        <FaGraduationCap size='20%' color="B3E5FC"/>
+                        <FaGraduationCap size='100%' color="B3E5FC"/>
                     </a>
                 </div>
                 <div className="Clock11">
                     <a href="https://www.audible.com.au/" target="_blank">
-                        <FaBook size='15%'color="B3E5FC"/>
+                        <FaBook size='100%'color="B3E5FC"/>
                     </a>
                 </div>
                 </div>
