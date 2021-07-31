a4e0a2da0327b959f09bb789e02c827d57aa1533
osamakashif <osamakashif1@hotmail.com>
2021-07-31 19:57:48 +1200

    feat - Implemented the list generation; fix - Fixed list formatting
    Implemented list generation; List component from ListItem components.
    Fixed formatting of ListItem.

--- front/src/components/List.js
+++ front/src/components/List.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import ListItem from './ListItem';
+import './List.css'
 
-const List = ({ list = "" }) => {
+const List = ({ list = [] }) => {
     return (
-        <div className="WastingChoice">
-            <header className="WastingChoice-header">
-                <h1>Hi {list}</h1>
-            </header>
-            <h1>What would you like to do today?</h1>
-            <NavLink to="/Wasting_Time"><button className="WastingOptions" id="OnlyWaste">Waste Time</button></NavLink>
-            <button className="WastingOptions" id="Efficient">Waste Time <i>efficiently</i></button>
+        <div className="List">
+            {list && list.map((listItem, index) => <ListItem item={listItem}/>)}
         </div>
     );
 }
--- front/src/components/ListItem.js
+++ front/src/components/ListItem.js
@@ -4,10 +4,13 @@ import './ListItem.css'
 
 const ListItem = ({ item = "" }) => {
     return (
-        <div>
-            <p>{item}&#160;&#160;&#160;&#160;<IoMdTrash className="Trash"/> </p> 
+        <div className="ListItem">
+            <p>{item}</p>
+            <IoMdTrash className="Trash" />
+            {/* <p>{item}&#160;&#160;&#160;&#160;<IoMdTrash className="Trash"/> </p>  */}
+
             {/* &#160; or &nbsp; are non-breakable spaces as HTML is whitespace collapsible. 4 used to act like a tab. */}
-        </div>
+        </div >
     );
 }
 
