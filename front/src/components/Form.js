a3349b57e79eaf65be3589753a1daa190047f337
osamakashif <osamakashif1@hotmail.com>
2021-08-01 13:19:52 +1200

    style - Fixed file formatting; fix - Fixed filename for CSS.
    Fixed formatting on Form.js.
    Fixed filename for CSS reference for Form.

--- front/src/components/Form.js
+++ front/src/components/Form.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import List from './List'
-import './List.css'
-import {v4 as uuidv4} from 'uuid';
+import './Form.css'
+import { v4 as uuidv4 } from 'uuid';
 
 const Form = ({ input, setInput, list, setList }) => {
     const onInputChange = (event) => {
@@ -10,16 +10,15 @@ const Form = ({ input, setInput, list, setList }) => {
 
     const onFormSubmit = (event) => {
         event.preventDefault();
-       setList([...list, {id: uuidv4(), title: input}]);
+        setList([...list, { id: uuidv4(), title: input }]);
     };
 
     return (
-            <form className="List" onSubmit={onFormSubmit}>
-                <List list={list} setList={setList}/>
-                <input type="text" placeholder="Enter an activity to procrastinate with... ;)" value={input} required onChange={onInputChange} />
-                <button type="submit">Add</button>
-            </form>
-
+        <form className="List" onSubmit={onFormSubmit}>
+            <List list={list} setList={setList} />
+            <input type="text" placeholder="Enter an activity to procrastinate with... ;)" value={input} required onChange={onInputChange} />
+            <button type="submit">Add</button>
+        </form>
     );
 }
 
