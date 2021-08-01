352a60b85e84b2de9e86820f14ea12d2269027e5
osamakashif <osamakashif1@hotmail.com>
2021-08-01 13:45:35 +1200

    style - fixed styling; chore - removed unnecessary stuff
    Fixed styling for Form.
    Removed unnecessary stuff from ListItem.

--- front/src/components/ListItem.js
+++ front/src/components/ListItem.js
@@ -2,19 +2,18 @@ import React from 'react';
 import { IoMdTrash } from 'react-icons/io'
 import './ListItem.css'
 
-const ListItem = ({item, list, setList}) => {
-    
-    // const currentItem = item.item;
+const ListItem = ({ item, list, setList }) => {
+
     const deleteItem = () => {
         let id = item.id;
-        let updatedList = list.filter((listItem) => listItem.id !== id );
+        let updatedList = list.filter((listItem) => listItem.id !== id);
         setList(updatedList);
     }
     return (
-        <li className="ListItem" key={item.id}>
+        <div className="ListItem" key={item.id}>
             <p>{item.title}</p>
-            <IoMdTrash className="Trash" onClick={deleteItem}/>
-        </li >
+            <IoMdTrash className="Trash" onClick={deleteItem} />
+        </div >
     );
 }
 
